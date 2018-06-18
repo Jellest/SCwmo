130b175634e9cc2aeb0b3a36c0314aed5e284209
jellest <jellejstuurman@gmail.com>
2018-06-18 11:56:34 +0200

    test

--- data-raw/dataraw.R
+++ data-raw/dataraw.R
@@ -6,9 +6,11 @@ AWS.df$IT_DATETIME<-as.Date(AWS.df$IT_DATETIME,format="%Y%m%d_240000_000000")
 
 devtools::use_data(AWS.df,overwrite=TRUE)
 
-AWS.sp<-AWS.df
+AWS.sp<-data.frame(AWS.df)
 coordinates(AWS.sp)<-~DS_LAT+DS_LON
-  
+crs(AWS.sp)<-CRS("+init=epsg:4326")
+
+# AWS.RD<-spTransform(AWS.sp,CRS("+init=epsg:28992"))  #rgdal wil niet installeren op pc132090
 # library(lubridate)
 # start<-as.Date("2010-01-01")
 # stop<-as.Date("2010-12-31")
