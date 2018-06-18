a9846b031d47f9087cb31bdb2233dc78ae320cba
jellest <jellejstuurman@gmail.com>
2018-06-18 12:17:58 +0200

    wrong commit

--- data-raw/dataraw.R
+++ data-raw/dataraw.R
@@ -8,7 +8,9 @@ devtools::use_data(AWS.df,overwrite=TRUE)
 
 AWS.sp<-data.frame(AWS.df)
 coordinates(AWS.sp)<-~DS_LAT+DS_LON
-crs(AWS.sp)<-CRS("+init=epsg:4326")
+crs(AWS.sp)<-CRS("+init=epsg:4326") #coordinaten kloppen wss niet of iets anders gaat mis
+
+devtools::use_data(AWS.sp,overwrite=TRUE)
 
 # AWS.RD<-spTransform(AWS.sp,CRS("+init=epsg:28992"))  #rgdal wil niet installeren op pc132090
 # library(lubridate)
