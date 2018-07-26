0d259c48e9e88ebf82b758ac1d0cdca2a9c9a628
jellest <jellejstuurman@gmail.com>
2018-07-26 17:09:53 +0200

    bgt import tryout

--- inst/notepad.R
+++ inst/notepad.R
@@ -1,1 +1,25 @@
 #notepad
+#load wfs
+
+library(gdalUtils)
+
+dsn <- "WFS:https://geodata.nationaalgeoregister.nl/beta/bgt/wfs?request=getcapabilities"
+
+ogrinfo(dsn, so=TRUE)
+
+ogr2ogr(dsn, "bgt.shp", "bgt:pand")
+
+library(rgdal)
+
+bgt <- readOGR("bgt.shp", "bgt", stringsAsFactors=FALSE)
+plot(bgt, max.plot = 19)
+
+library(rwfs)
+fileName <- tempfile()
+download.file("https://geodata.nationaalgeoregister.nl/beta/bgt/wfs?request=getcapabilities", fileName)
+request <- rwfs::GMLFile$new(fileName)
+client <- rwfs::WFSCachingClient$new(request)
+layer <- client$getLayer("bgt:pand")
+print(layer@data)
+plot(layer, max.plot = 19)
+unlink(fileName)
