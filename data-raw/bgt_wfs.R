373ea4b634b381c6706d1a68467486089f5e018f
jellest <jellejstuurman@gmail.com>
2018-08-09 13:48:22 +0200

    load BGT wfs

---
+++ data-raw/bgt_wfs.R
@@ -0,0 +1,16 @@
+##load BGT
+
+#load wfs
+library(gdalUtils)
+bgt_wfs <- "WFS:https://geodata.nationaalgeoregister.nl/beta/bgt/wfs?request=getcapabilities"
+
+#ogrinfo(bgt_wfs, so=TRUE)
+
+#load pand layer and make shapefile
+ogr2ogr(dsn, "bgt.shp", "bgt:pand")
+library(rgdal)
+on
+
+#plot BGT
+bgt <- readOGR("bgt.shp", "bgt", stringsAsFactors=FALSE)
+plot(bgt, max.plot = 19)
