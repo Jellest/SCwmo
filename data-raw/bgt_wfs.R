09778f1f57aa2140bca89729caacab8938313386
jellest <jellejstuurman@gmail.com>
2018-08-14 16:49:25 +0200

    import bgt wfs

--- data-raw/bgt_wfs.R
+++ data-raw/bgt_wfs.R
@@ -3,11 +3,18 @@ library(rgeos)
 library(rgdal)
 library(gdalUtils)
 
+dataset <- "BGT"
+
+
+#De Bilt
+station <- "DeBilt"
 debilt<-data.frame("lat"=140802.698, "lon"=456881.8)
 coordinates(debilt)<-~lat+lon
 
 #create 200 meter buffer arouond sensor
 surroundingBuffer <- buffer(debilt,width=200) #since RDcoords are in meters width is also in m
+
+data_location <- paste("/nobackup/users/stuurman/data/", dataset, station, sep="")
 
 #get BBOX extent of buffer area
 surroundingExtent <- extent(surroundingBuffer)
@@ -18,7 +25,6 @@ my_EPSG <- "EPSG:28992"
 my_bbox <- paste(toString(surroundingExtent@xmin), toString(surroundingExtent@ymin), toString(surroundingExtent@xmax), toString(surroundingExtent@ymax), sep=",")
 bgt_wfs <- paste("WFS:", bgt_wfsBaseUrl, "&SRSNAME=", my_EPSG, "&BBOX=", my_bbox, sep="")
 
-
 #ogrinfo(bgt_wfs, so=TRUE)
 
 #list of features
@@ -26,28 +32,18 @@ ogrListLayers(bgt_wfs)
 bgtFeatureNamesList <- c("bgt:begroeidterreindeel", "bgt:functioneelgebied", "bgt:onbegroeidterreindeel", "bgt:ondersteunendwaterdeel", "bgt:waterdeel", "bgt:ondersteunendwegdeel", "bgt:wegdeel", "bgt:pand", "bgt:spoor")
 featureNamesList <- c("begroeidterreindeel", "functioneelgebied", "onbegroeidterreindeel","ondersteunendwaterdeel", "waterdeel", "ondersteunendwegdeel", "wegdeel", "pand", "spoor")  
 
-#define features to be loaded
-bgtShpList <- list()
-
-for (i in seq_along(featureNamesList)){
-  feature_name = paste("feature",featureNamesList[i], sep="_")
-  #assign(paste("Feature", bgtFeatureNamesList[i], sep = "_"), SpatialPolygonsDataFrame(SpatialPolygons(list()), data=data.frame()))
+read_bgt<-function(bgt_feature,feature_name_short, storage_location){
+  feature_name = paste("feature",bgt_feature, sep="_")
   feature = assign(feature_name, SpatialPolygonsDataFrame(SpatialPolygons(list()), data=data.frame()))
-  #bgtShpList[feature_name]<- feature
-}
-for(j in seq_along(bgtFeatureNamesList)){
 
-  #layer_name = paste("bgt",bgtFeatureNamesList[j],sep= ":")
-  #print(layer_name)
-  print(featureNamesList[j])
-  print(bgtFeatureNamesList[j])
+  print(feature_name)
+  shape_file  = paste(storage_location, feature_name_short,".shp",sep="")
+
+  ogr2ogr(src_datasource_name = bgt_wfs, dst_datasource_name = shape_file, layer = feature_name_short)
   
-  shape_file  = paste(featureNamesList[j],".shp",sep="")
-  ogr2ogr(bgt_wfs, shape_file, bgtFeatureNamesList[j])
-  bgtShpList[j] <- readOGR(shape_file, featureNamesList[j], stringsAsFactors=FALSE)
+  bgtShpList <- tryCatch(readOGR(dsn = shape_file, layer = feature_name_short, storage_location, stringsAsFactors=FALSE),error=function(e) e)
+  
+  return(bgtShpList)
 }
 
-#load pand layer and make shapefile
-ogr2ogr(bgt_wfs, "bgt.shp", "bgt:functioneelgebied")
-bgtTest <- readOGR("bgt.shp", "bgt", stringsAsFactors=FALSE)
-plot(bgtTest)
+bgt_list<-mapply(read_bgt,bgt_feature = bgtFeatureNamesList, feature_name_short = featureNamesList, storage_location = data_location)
