3666d45d984d2a6b3bedbd6fd22aa6dfde803be1
Jelle Stuurman <jellejstuurman@gmail.com>
2018-09-09 22:33:15 +0200

    finalized import bgt

--- R/import_bgt.R
+++ R/import_bgt.R
@@ -63,99 +63,160 @@ bgt_begroeidterrein_features[complete.cases(bgt_begroeidterrein_features),]
 
 all_bgt_objects_200m <- data.frame(aws = character(0), feature_type = character(0), has_features = logical(0), features_count = numeric(0), stringsAsFactors = FALSE) 
 
-bgt_colNames <- c("bgt-functi", "bgt-fysiek", "bgt-type", "bronhouder", "gml_id", "object_type", "plus-fun_1", "plus-fun_2", "plus-funct", "plus-fys_2", "plus-fysie", "plus-sta_1", "plus-sta_2", "plus-statu", "plus-typ_1", "plus-type", "relatieveh", "status")
-bgt.df <- setNames(data.frame(matrix(ncol = 18, nrow = 0), stringsAsFactors = FALSE), bgt_colNames)
+bgt_colNames <- c("bgt-functi", "bgt-fysiek", "bgt-type", "bronhouder", "gml_id", "naam", "object_type", "plus-fun_1", "plus-fun_2", "plus-funct", "plus-fys_2", "plus-fysie", "plus-sta_1", "plus-sta_2", "plus-statu", "plus-typ_1", "plus-type", "relatieveh", "status")
+bgt.df <- setNames(data.frame(matrix(ncol = 19, nrow = 0), stringsAsFactors = FALSE), bgt_colNames)
 bgt.df <- data.frame(lapply(bgt.df, function(x) if(is.logical(x)) {return(as.character(x))} else { return(x)}), stringsAsFactors=FALSE)
 
-assign(paste("BGT", station, sep="_"), SpatialPolygonsDataFrame(SpatialPolygons(list()), data=bgt.df), envir = .GlobalEnv)
+#assign(paste("BGT", station, sep="_"), SpatialPolygonsDataFrame(SpatialPolygons(list()), data=bgt.df), envir = .GlobalEnv)
 #bgt_shape <- assign(paste("BGT", station, sep="_"), st_sfc(st_polygon(list())))
 
-crs(BGT_deBilt) <- crs("+init=epsg:28992")
+#crs(BGT_deBilt) <- crs("+init=epsg:28992")
 
 
-#
-addColumns <- function(raw_data_location, object_name_short){
+#adjust columns
+adjustColumns <- function(raw_data_location, object_name_short){
   print("adding columns...")
   shp <- readOGR(dsn = raw_data_location, layer = object_name_short, stringsAsFactors=FALSE)
-  
-  ## add the different mising columns to proceed later with correct rbind
+  print(paste("before", length(names(shp))))
+  ## add and remove the different columns to proceed later with correct rbind
+  print(paste("in: ", object_name_short))
   if(object_name_short == "begroeidterreindeel"){
-    shp@data["plus-funct"] <- c()
-    shp@data["plus-fun_1"] <- c()
-    shp@data["plus-fun_2"] <- c()
-    shp@data["bgt-functi"] <- c()
-    shp@data["plus-typ_1"] <- c()
-    shp@data["plus-type"] <- c()
-    shp@data["bgt-type"] <- c()
-  } else if(object_name_short == "onbegroeidterreindeel"){
-    shp@data["plus-funct"] <- c()
-    shp@data["plus-fun_1"] <- c()
-    shp@data["plus-fun_2"] <- c()
-    shp@data["bgt-functi"] <- c()
-    shp@data["plus-typ_1"] <- c()
-    shp@data["plus-type"] <- c()
-    shp@data["bgt-type"] <- c()
+    shp$plus.funct <- NA
+    shp$plus.fun_1 <- NA
+    shp$plus.fun_2 <- NA
+    shp$bgt.functi <- NA
+    shp$plus.typ_1 <- NA
+    shp$plus.type <- NA
+    shp$bgt.type <- NA
+    shp$naam <- NA
+    shp$object_typ <- "begroeidterreindeel" 
+    
+    drops<- c('status_lee', 'status_cod', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'optalud_le', 'fysiekvoor', 'creationda', 'terminat_1', 'plus.fys_1', 'optalud', 'lv.publica', 'tijdstipre', 'kruinlijn_')
+    shp <- shp[,!(names(shp) %in% drops)]
+  
+    } else if(object_name_short == "onbegroeidterreindeel"){
+    shp$plus.funct <- NA
+    shp$plus.fun_1 <- NA
+    shp$plus.fun_2 <- NA
+    shp$bgt.functi <- NA
+    shp$plus.typ_1 <- NA
+    shp$plus.type <- NA
+    shp$bgt.type <- NA
+    shp$naam <- NA
+    shp$object_typ <- "onbegroeidterreindeel" 
+    
+    drops<- c('status_lee', 'status_cod', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'optalud_le', 'fysiekvoor', 'creationda', 'terminat_1', 'plus.fys_1', 'optalud', 'lv.publica', 'tijdstipre', 'kruinlijn_')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
   } else if(object_name_short == "waterdeel"){
-    shp@data["plus-fysie"] <- c()
-    shp@data["plus-funct"] <- c()
-    shp@data["plus-fun_1"] <- c()
-    shp@data["bgt-fun_2"] <- c()
-    shp@data["bgt-functi"] <- c()
-    shp@data["plus-fysiek"] <- c()
-    shp@data["plus-fys_2"] <- c() 
+    shp$plus.fysie <- NA
+    shp$plus.funct <- NA
+    shp$plus.fun_1 <- NA
+    shp$plus.fun_2 <- NA
+    shp$bgt.functi <- NA
+    shp$bgt.fysiek <- NA
+    shp$plus.fys_2 <- NA
+    shp$naam <- NA
+    shp$object_typ <- "waterdeel" 
+    
+    drops<- c('type_codes', 'status_lee', 'status_cod', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'creationda', 'terminat_1', 'lv.publica', 'tijdstipre', 'plus.type_')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
   } else if(object_name_short == "wegdeel"){
-    shp@data["plus-typ_1"] <- c()
-    shp@data["plus-type"] <- c()
-    shp@data["bgt-type"] <- c() 
+    shp$plus.typ_1 <- NA
+    shp$plus.type <- NA
+    shp$bgt.type <- NA
+    shp$naam <- NA
+    shp$object_typ <- "wegdeel" 
+    
+    drops<- c('status_lee', 'status_cod', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'plus.fys_1', 'lv.publica', 'kruinlijn_', 'tijdstipre', 'optalud', 'creationda', 'optalud_le', 'fysiekvoor', 'terminat_1', 'functie_co')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
   } else if(object_name_short == "scheiding"){
-    shp@data["plus-fysie"] <- c()
-    shp@data["plus-functplus-fun_1"] <- c()
-    shp@data["plus-fun_2bgt-functi"] <- c()
-    shp@data["bgt-fysiek"] <- c()
-    shp@data["plus-fys_2"] <- c()
+    shp$plus.fysie <- NA
+    shp$plus.funct <- NA
+    shp$plus.fun_1 <- NA
+    shp$plus.fun_2 <- NA
+    shp$bgt.functi <- NA
+    shp$bgt.fysiek <- NA
+    shp$plus.fys_2 <- NA
+    shp$naam <- NA
+    shp$object_typ <- "scheiding" 
+    
+    drops<- c('type_codes', 'status_lee', 'status_cod', 'plus.type_', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'creationda', 'terminat_1', 'lv.publica', 'tijdstipre')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
   } else if(object_name_short == "pand"){
-    shp@data["plus-fysie"] <- c()
-    shp@data["plus-funct"] <- c()
-    shp@data["plus-fun_1"] <- c()
-    shp@data["plus-fun_2"] <- c()
-    shp@data["bgt-functi"] <- c()
-    shp@data["bgt-fysiek"] <- c()
-    shp@data["plus-fys_2"] <- c()
-    shp@data["plus-typ_1"] <- c()
-    shp@data["plus-type"] <- c()
-    shp@data["bgt-type"] <- c()
-  } else if(object_name_short == ""){
-    shp@data["plus-fysie"] <- c()
-    shp@data["plus-funct"] <- c()
-    shp@data["plus-fun_1"] <- c()
-    shp@data["plus-fun_2"] <- c()
-    shp@data["bgt-functi"] <- c()
-    shp@data["bgt-fysiek"] <- c()
-    shp@data["plus-fys_2"] <- c()
-    shp@data["plus-typ_1"] <- c()
-    shp@data["plus-type"] <- c()
-    shp@data["bgt-type"] <- c()
+    shp$plus.fysie <- NA
+    shp$plus.funct <- NA
+    shp$plus.fun_1 <- NA
+    shp$plus.fun_2 <- NA
+    shp$bgt.functi <- NA
+    shp$bgt.fysiek <- NA
+    shp$plus.fys_2 <- NA
+    shp$plus.typ_1 <- NA
+    shp$plus.type <- NA
+    shp$bgt.type <- NA
+    shp$naam <- NA
+    shp$object_typ <- "pand" 
+    
+    drops<- c('identifica', 'status_lee', 'status_cod', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'creationda', 'terminat_1', 'lv.publica', 'tijdstipre')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
+  } else if(object_name_short == "overigbouwwerk"){
+    shp$plus.fysie <- NA
+    shp$plus.funct <- NA
+    shp$plus.fun_1 <- NA
+    shp$plus.fun_2 <- NA
+    shp$bgt.functi <- NA
+    shp$bgt.fysiek <- NA
+    shp$plus.fys_2 <- NA
+    shp$plus.typ_1 <- NA
+    shp$plus.type <- NA
+    shp$bgt.type <- NA
+    shp$naam <- NA
+    shp$object_typ <- "overigbouwwerk" 
+    
+    drops<- c('type_codes', 'plus.type_', 'identifica', 'status_lee', 'status_cod', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'creationda', 'terminat_1', 'lv.publica', 'tijdstipre')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
   } else if(object_name_short == "spoor"){
-    shp@data["plus-fysie"] <- c()
-    shp@data["bgt-fysiek"] <- c()
-    shp@data["plus-fys_2"] <- c()
-    shp@data["plus-typ_1"] <- c()
-    shp@data["plus-type"] <- c()
-    shp@data["bgt-type"] <- c()
-  } else if(object_name_short == ""){
-    shp@data["plus-fysie"] <- c()
-    shp@data["plus-funct"] <- c()
-    shp@data["plus-fun_1"] <- c()
-    shp@data["plus-fun_2"] <- c()
-    shp@data["bgt-functi"] <- c()
-    shp@data["bgt-fysiek"] <- c()
-    shp@data["plus-fys_2"] <- c()
-    shp@data["plus-typ_1"] <- c()
-    shp@data["plus-type"] <- c()
+    shp$plus.fysie <- NA
+    shp$bgt.fysiek <- NA
+    shp$plus.fys_2 <- NA
+    shp$plus.typ_1 <- NA
+    shp$plus.type <- NA
+    shp$bgt.type <- NA
+    shp$naam <- NA
+    shp$object_typ <- "spoor" 
+    
+    drops<- c('status_lee', 'status_cod', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'functie_co', 'creationda', 'terminat_1', 'lv.publica', 'tijdstipre')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
+  } else if(object_name_short == "functioneelgebied"){
+    shp$plus.fysie <- NA
+    shp$plus.funct <- NA
+    shp$plus.fun_1 <- NA
+    shp$plus.fun_2 <- NA
+    shp$bgt.functi <- NA
+    shp$bgt.fysiek <- NA
+    shp$plus.fys_2 <- NA
+    shp$plus.typ_1 <- NA
+    shp$plus.type <- NA
+    shp$object_typ <- "functioneelgebied" 
+    
+    drops<- c('type_codes', 'status_lee', 'status_cod', 'plus.type_', 'naam', 'inonderzoe', 'eindregist', 'terminatio', 'lokaalid', 'inonderz_1', 'creationda', 'terminat_1', 'naam_leeg', 'lv.publica', 'plus.typ_1', 'plus.type', 'tijdstipre')
+    shp <- shp[,!(names(shp) %in% drops)]
+    
   }
   shp[ , order(names(shp))]
-  
-return (shp)}
+  print(paste("after", length(names(shp))))
+  print(names(shp))
+  writeOGR(obj = shp, dsn = raw_data_location, layer = object_name_short, driver = "ESRI Shapefile", overwrite_layer = TRUE)
+  return (shp)
+}
+
+bg_counter <- 0
 read_bgt<-function(aws, wfs, bgt_object_name, object_name_short){
   bgt_directory <- paste("data", "BGT", sep=folder_structure)
   dir.create(paste(bgt_directory, station, sep=folder_structure), showWarnings = FALSE)
@@ -178,18 +239,31 @@ read_bgt<-function(aws, wfs, bgt_object_name, object_name_short){
     ogr2ogr(src_datasource_name = wfs    , dst_datasource_name = shape_file, layer = bgt_object_name, overwrite = TRUE)
     
     object_name <- readOGR(dsn = raw_data_location, layer = object_name_short, stringsAsFactors=FALSE)
-    object_name@data$object_type<- c(object_name_short)
+    #object_name@data$object_type<- c(object_name_short)
     crs(object_name) <- crs("+init=epsg:28992")
-    #proj4string(object_name) <- CRS("+init=epsg:28992")
-    
-    #rbind
-    object_name <- addColumns(raw_data_location, object_name_short)
-    crs(object_name) <- crs("+init=epsg:28992")
-    print(crs(object_name))
-    print(str(BGT_deBilt))
-    print(crs(BGT_deBilt))
-    rbind(BGT_deBilt, object_name, makeUniqueIDs = TRUE)
 
+    #adjust columns
+    object_name <- adjustColumns(raw_data_location, object_name_short)
+    print(paste("field count object name: ", length(names(object_name@data))))
+
+    crs(object_name) <- crs("+init=epsg:28992")
+    
+    bgt_counter <<- bgt_counter + 1
+    
+    if(bgt_counter == 1){
+      print("in 1")
+      #print(length(object_name))
+      BGT_station.sdf <<- object_name
+      #print(length(BGT_shape))
+    } else {
+      print("in >1")
+      print(paste("length new object:",length(object_name), sep=" "))
+      print(paste("length BGT:",length(BGT_station.sdf), sep=" "))
+      
+      
+      BGT_station.sdf <<- rbind(BGT_station.sdf, object_name, makeUniqueIDs = TRUE)
+      print(paste("new length BGT:",length(BGT_station.sdf), sep=" "))
+    }
     object_count <- length(object_name)
     entry_t <- data.frame(aws, object_name_short, TRUE, object_count, stringsAsFactors=FALSE)
     names(entry_t) <- c("aws", "object_type", "has_features", "feature_count")
@@ -197,6 +271,7 @@ read_bgt<-function(aws, wfs, bgt_object_name, object_name_short){
     all_bgt_objects_200m <<- rbind(all_bgt_objects_200m, entry_t, stringsAsFactors=FALSE)
     
   }, error=function(e){
+    bgt_counter <<- bgt_counter + 1
     object <- NA
     #remove empty shp
     shp <- paste(raw_shape_file_path, object_name_short, ".shp", sep="")
@@ -215,17 +290,30 @@ read_bgt<-function(aws, wfs, bgt_object_name, object_name_short){
     object_name <- NA
   }
   )
+  
+  if(bgt_counter == length(bgt_objects_name_list)){
+    shp_name <- paste("BGT", station, sep="_")
+    writeOGR(obj = BGT_station.sdf, dsn = working_directory, layer = shp_name, driver = "ESRI Shapefile", overwrite_layer = TRUE)
+    remove(BGT_station.sdf)
+  }
   return (object_name)
 }
 
 #create list of shape files
+bgt_counter <- 0
+remove(BGT_station.sdf)
 bgt_list <- list()
-bgt_list <- mapply(read_bgt,aws = aws, wfs = bgt_wfs, bgt_object_name = bgt_objects_name_list[1], object_name_short = bgt_objects_shortname_list[1])
+bgt_list <- mapply(read_bgt,aws = aws, wfs = bgt_wfs, bgt_object_name = bgt_objects_name_list, object_name_short = bgt_objects_shortname_list)
 
 bgt_shp_files <- list.files(paste("data", "BGT", station, "raw", sep=folder_structure), pattern = "\\.shp$")
 
 names(bgt_list) <- bgt_objects_shortname_list
-pand_shape <- bgt_list[["pand"]]
+pand_shape <- bgt_list[pand]
 crs(pand_shape)<- CRS("+init=epsg:28992")
 crs(pand_shape)
 crs(bgt_shape)
+
+desk <- "C:/users/Jelle/Desktop"
+mshp<- "C:/users/Jelle/Desktop/test.shp"
+ogr2ogr(src_datasource_name = desk    , dst_datasource_name = mshp, layer = 'pand', overwrite = TRUE)
+
