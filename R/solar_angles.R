9233259a5c3eebf39f42b45aadfc047fc274ba04
jellest <jellejstuurman@gmail.com>
2018-09-21 18:09:17 +0200

    completed solar angles chart

--- R/solar_angles.R
+++ R/solar_angles.R
@@ -1,5 +1,6 @@
 library(scatterplot3d)
 library(lubridate)
+library(insol)
 #'Calculating the solar angles for AWS locations
 #'
 #'@title solar angles
@@ -15,29 +16,53 @@ library(lubridate)
 #'solar_angles(jd=seq(1,20,by=0.1)) #for the first 20 days with an accuracy of approximately 2.5 hours
 #'@return dataframe with variables lat (latitude), lon (longitude), elv (elevation), jd (julian day), azimuth, zenith,... 
 #'@export
-solar_angles<-function(lon, lat, elv, julian_day){
+solar_angles<-function(lon, lat, altitude, julian_day){
   requireNamespace("insol")
-  
-
   
   sun_vector <- sunvector(julian_day,lat,lon,1)
   sun_position <- sunpos(sun_vector)
   summary(sun_position)
   
+  m <- cbind(lat, lon,  altitude, julian_day, sun_position)
+  df <- data.frame(m)
+  df$elevation <- 90 - df$zenith
+  df_ah <- subset(df, zenith < 90)
   
-  df <- cbind(lat, lon,  elv, julian_day, sun_position)
-  #df["altitude"] <- 90 - df["zenith"]
-  return(df)
+  return(list(df, df_ah))
 }
-intervals <- JD(seq(ISOdate(2017,01,01,0),ISOdate(2017,12,31,23),by='60 min'))
-angles <- solar_angles(lon = aws_debilt_wgs.sp@coords[,"lon"],
-                       lat = aws_debilt_wgs.sp@coords[,"lat"],
-                       elv= 42.70,
-                       julian_day = intervals)
-altitudeangle <- function(sunposition){
-  altitude_angle <- 90 - sunposition["zenith"]
-  sunposition["altitude"] <- altitude_angle
-  return (sunposition)}
 
-sapply(angles, altitudeangle)
-plot()
+# all_solar_angles <- data.frame(solar_angles(lon = aws_debilt_wgs.sp@coords[,"lon"],
+#                        lat = aws_debilt_wgs.sp@coords[,"lat"],
+#                        altitude = 42.70,
+#                        julian_day = intervals))
+# all_solar_angles$elevation <- 90 - all_solar_angles$zenith
+# above_horizon_solar_angles <- subset(all_solar_angles, zenith < 90)
+
+julian_day_hour <- function(year,month,day, s_hour, f_hour){
+  if(missing(s_hour)){
+    s_hour <- 0
+  }
+  if(missing(f_hour)){
+    f_hour <- 23
+  }
+  jd <- JD(seq(ISOdate(year,month,day,s_hour),ISOdate(year,month,day,f_hour),by='60 min'))
+  return(jd)
+}
+
+month_solar_angles <- list()
+for (m in 1:6){
+  month_solar_angles[[m]] <- data.frame(solar_angles(lon = aws_debilt_wgs.sp@coords[,"lon"],
+                                                     lat = aws_debilt_wgs.sp@coords[,"lat"],
+                                                     altitude = 42.70,
+                                                     julian_day = julian_day_hour(2017, m, 21))[1]) 
+}
+month_solar_angles[[7]] <- data.frame(solar_angles(lon = aws_debilt_wgs.sp@coords[,"lon"],
+                                                   lat = aws_debilt_wgs.sp@coords[,"lat"],
+                                                   altitude = 42.70,
+                                                   julian_day = julian_day_hour(2017, 12, 21))[1])
+
+plot(month_solar_angles[[1]]$azimuth, month_solar_angles[[1]]$elevation, type="l", xlim=c(0,360), ylim=c(0,70), xlab="azimuth", ylab="elevation")
+for (p in 2:length(month_solar_angles)){
+  par(new=T)
+  plot(month_solar_angles[[p]]$azimuth, month_solar_angles[[p]]$elevation, type="l",  xlim=c(0,360), ylim=c(0,70), axes = FALSE, xlab="", ylab="")
+}
