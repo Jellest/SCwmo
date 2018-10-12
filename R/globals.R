9b08717d64a54018a196c612b6d8a404cd6f3840
Jelle Stuurman <jellejstuurman@gmail.com>
2018-10-12 10:43:57 +0200

    load all librariesthrough function

--- R/globals.R
+++ R/globals.R
@@ -2,3 +2,17 @@
 
 # Load computer config first. See inst? computer_config in inst>computer_config.R
 
+libs <- function(){
+  library(raster)
+  library(rgeos)
+  library(rgdal)
+  library(gdalUtils)
+  library(gdata)
+  library(sp)
+  library(sf)
+  library(lubridate)
+  library(insol)
+  library(ggplot2)
+  library(leaflet)
+  library(mapview)
+}
