24b402a7ed90a39622ea4d3bc88f4c8d37b90100
Jelle Stuurman <jellejstuurman@gmail.com>
2019-01-09 00:21:09 +0100

    improved and cleanup

--- R/import_ahn2.R
+++ R/import_ahn2.R
@@ -1,12 +1,16 @@
-import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, terrain_ahn, AHN3, remove_sheets){
+import_ahn <- function(aws_name, station_coords, X, Y, LONLAT, resolution, radius, raw_ahn, terrain_ahn, AHN3, remove_sheets){
   aws_name_trim <- getAWS_name_trim(aws_name)
   if(missing(resolution)){
+    message("No correct resolution is found. Resolution of 0.5 m will be used.")
     resolution_name <- "05m"
   } else {
     if(resolution == 0.5){
       resolution_name <- "05m"
     } else if(resolution == 5){
       resolution_name <- "5"
+    } else {
+      warning("No correct resolution is found. Resolution of 0.5 m will be used.")
+      resolution_name <- "05m"
     }
   }
   
@@ -29,14 +33,32 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
     remove_sheets = FALSE
   }
   
-  #create radius buffer arouond sensor
+  ##create radius buffer around sensor
+  #check provided point or coordinates
+  if(missing(LONLAT)){
+    LONLAT = FALSE
+  }
+  
+  if(missing(station_coords)){
+    station_coords <- data.frame("X"=X,"Y"=Y)
+    coordinates(station_coords) <- ~X+Y
+    if(LONLAT == TRUE){
+      crs(station_coords) <- CRS("+init=epsg:4326")
+    } else {
+      crs(station_coords) <- CRS("+init=epsg:28992")
+    }
+  }
+  
+  if(LONLAT == TRUE){
+    station_coords <- spTransform(station_coords, CRS = CRS("init:espg28992"))
+  }
+  
   point.sf <- st_as_sf(station_coords)
-  surroundingBuffer <<- st_buffer(point.sf,dist=radius) #since RDcoords are in meters width is also in m
-  surroundingBuffer.sp <<- buffer(station_coords, width=radius)
+  surroundingBuffer <- st_buffer(point.sf,dist=radius) #since RDcoords are in meters width is also in m
   #get BBOX extent of buffer area
   surroundingExtent <- extent(surroundingBuffer)
   my_EPSG <- "EPSG:28992"
-  my_bbox <- paste(toString(surroundingExtent@xmin), toString(surroundingExtent@ymin), toString(surroundingExtent@xmax), toString(surroundingExtent@ymax), sep=",")
+  #my_bbox <- paste(toString(surroundingExtent@xmin), toString(surroundingExtent@ymin), toString(surroundingExtent@xmax), toString(surroundingExtent@ymax), sep=",")
   
   #get AHN bladIndex
   ahn_directory <- paste("data", AHN, sep="/")
@@ -64,20 +86,6 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
   bladIndex_buffer_intsct.sf <- st_intersection(bladIndex.sf, surroundingBuffer)
   
   bladnrs <<- bladIndex_buffer_intsct.sf$bladnr
-  # if(AHN == "AHN3"){
-  #   upperbladnrs <- list()
-  #   for (b in 1:length(bladnrs)){
-  #     upperbladnrs <- c(upperbladnrs, toupper(bladnrs[b]))
-  #   }
-  #   bladnrs <<- upperbladnrs
-  # }
-  #bladnrs_split <- strsplit(bladnrs, " ")
-  #print(paste("bladnrs:",bladnrs_split[1]))
-  #gml_ids <- bladIndex.shp$gml_id
-  #View(gml_ids)
-  #blad_count <- length(gml_ids)
-  #gml_ids_split <- strsplit(gml_ids, " ")
-  #print(gml_ids_split)
   
   dir.create(paste(ahn_directory, aws_name_trim, sep="/"), showWarnings = FALSE)
   aws_working_directory <- paste(ahn_directory, aws_name_trim, sep="/")
@@ -85,16 +93,14 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
   ahn_atomFeed_BaseUrl <- paste("https://geodata.nationaalgeoregister.nl/", tolower(AHN), "/extract/", tolower(AHN), "_", sep="")
   #download raw ahn of corresponding resolultion
   indiv_raw_rasters <- list()
-  raw_intersects <<- list()
   if(raw_ahn == TRUE){  
     ahn_raw_directory <- paste(aws_working_directory, "raw", sep="/")
     if(!file.exists(paste(ahn_raw_directory, "/", aws_name_trim, "_raw_ahn", '.tif', sep=""))){
       dir.create(paste(aws_working_directory, "raw", sep="/"), showWarnings = FALSE)
       print(ahn_raw_directory)
-      rawXList <<- c()
-      rawYList <<- c()
+      # rawXList <<- c()
+      # rawYList <<- c()
       paste("Amount of sheets found:", length(bladnrs), sep=" ")
-      ahn_raw_file_paths <<- c()
       for(r in 1:length(bladnrs)){
         #ahn2: https://geodata.nationaalgeoregister.nl/ahn2/extract/ahn2_05m_ruw/r32cn1.tif.zip
         #ahn3: https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_05m_dsm/R_32CN1.zip
@@ -125,10 +131,10 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
         proj4string(ahn_sheet_raw)<-CRS("+init=epsg:28992") 
         ahn_raw_crop <- raster::crop(ahn_sheet_raw,surroundingBuffer)
         #plot(ahn_raw_crop)
-        rXs <- c(xmin(ahn_raw_crop), xmax(ahn_raw_crop))
-        rYs <- c(ymin(ahn_raw_crop), ymax(ahn_raw_crop))
-        rawXList <<- rbind(rawXList, rXs)
-        rawYList <<- rbind(rawYList, rYs)
+        # rXs <- c(xmin(ahn_raw_crop), xmax(ahn_raw_crop))
+        # rYs <- c(ymin(ahn_raw_crop), ymax(ahn_raw_crop))
+        # rawXList <- rbind(rawXList, rXs)
+        # rawYList <- rbind(rawYList, rYs)
         indiv_raw_rasters[[r]] <- ahn_raw_crop
       }
       
@@ -160,8 +166,8 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
   indiv_terrain_rasters <- list()
   if(terrain_ahn == TRUE){
     ahn_terrain_directory <- paste(aws_working_directory, "terrain", sep="/")
-    terrainXList <<- c()
-    terrainYList <<- c()
+    # terrainXList <<- c()
+    # terrainYList <<- c()
     if(!file.exists(paste(ahn_terrain_directory, "/", aws_name_trim, "_terrain_ahn", '.tif', sep=""))){
       dir.create(paste(aws_working_directory, "terrain", sep="/"), showWarnings = FALSE)
       print(ahn_terrain_directory)
@@ -185,17 +191,17 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
         print(ahn_terrain_downloadLink)
         ahn_terrainZip_file_path <- paste(ahn_terrain_directory, "/", ahn_terrain_letter, bladnrs[[t]], ".zip", sep="")
         if(!file.exists(ahn_terrain_file_path)){
-          ahn_terrain_zip <- download.file(ahn_terrain_downloadLink, destfile = ahn_terrainZip_file_path)
-          ahn_terrain <- unzip(ahn_terrainZip_file_path, overwrite = TRUE, exdir = ahn_terrain_directory)
+          download.file(ahn_terrain_downloadLink, destfile = ahn_terrainZip_file_path)
+          unzip(ahn_terrainZip_file_path, overwrite = TRUE, exdir = ahn_terrain_directory)
           file.remove(ahn_terrainZip_file_path)
         }
         ahn_sheet_terrain <-stack(ahn_terrain_file_path)
         proj4string(ahn_sheet_terrain)<-CRS("+init=epsg:28992") 
         ahn_terrain_crop<-raster::crop(ahn_sheet_terrain,surroundingBuffer)
-        tXs <- c(xmin(ahn_terrain_crop), xmax(ahn_terrain_crop))
-        tYs <- c(ymin(ahn_terrain_crop), ymax(ahn_terrain_crop))
-        terrainXList <<- rbind(terrainXList, tXs)
-        terrainYList <<- rbind(terrainYList, tYs)
+        # tXs <- c(xmin(ahn_terrain_crop), xmax(ahn_terrain_crop))
+        # tYs <- c(ymin(ahn_terrain_crop), ymax(ahn_terrain_crop))
+        # terrainXList <- rbind(terrainXList, tXs)
+        # terrainYList <- rbind(terrainYList, tYs)
         indiv_terrain_rasters[[t]] <- ahn_terrain_crop
       }
       
@@ -226,10 +232,13 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
   
   if(raw_ahn == TRUE & terrain_ahn == TRUE){
     ahn_rasters <- stack(ahn_raw_raster, ahn_terrain_raster)
+    names(ahn_rasters) <- c("raw", "terrain")
   } else if(raw_ahn == TRUE & terrain_ahn == FALSE){
     ahn_rasters <- stack(ahn_raw_raster)
+    names(ahn_rasters) <- c("raw")
   } else if(raw_ahn == FALSE & terrain_ahn == TRUE){
     ahn_rasters <- stack(ahn_terrain_raster)
+    names(ahn_rasters) <- c("terrain")
   } else {
     ahn_rasters <- NULL
   }
@@ -238,15 +247,5 @@ import_ahn <- function(aws_name, station_coords, resolution, radius, raw_ahn, te
 
 return (ahn_rasters)}
 
-test_raster <- import_ahn(aws_name = "De Bilt",
-                          station_coords = test_point.sp,#station_coords = selected_aws_temp[["aws_rd.sp"]],
-                          resolution = 0.5, radius = 500,
-                          raw_ahn = TRUE, terrain_ahn = TRUE,
-                          AHN3 = FALSE)
+
 #test_ahn_raster <- import_ahn2(aws_name = "De Bilt", station_coords = point.sf, resolution = 0.5, radius = 2000, raw_ahn = TRUE, terrain_ahn = FALSE)
-
-
-test_point.sp <- data.frame("X"=135000.000,"Y"=456250.000)
-coordinates(test_point.sp) <- ~X+Y
-crs(test_point.sp) <- CRS("+init=epsg:28992")
-test_point.sf <- st_as_sf(test_point.sp)
