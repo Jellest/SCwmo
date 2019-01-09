38ae484e7154a08290a3eb6553a59b644f538e59
Jelle Stuurman <jellejstuurman@gmail.com>
2019-01-09 22:24:35 +0100

    added test commands

--- inst/test_commands.R
+++ inst/test_commands.R
@@ -3,3 +3,9 @@
 test_ssa.csv <- fread("data/solar_shadow_angles/solar_shadow_angles.csv", data.table = FALSE)
 test_ssa_criteria <- projected_shade_class(test_ssa.csv)
 
+
+##import bgt
+BGT_DeBilt <- import_bgt("De Bilt", "temp_150cm")
+BGT_HvH <-import_bgt("Hoek van Holland", "site")
+
+BGT_DeBilt <- import_single_bgt("De Bilt", "site", delete_raw_gmls = TRUE)
