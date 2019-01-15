c608edd88e2fe936fa63a5b68d2937f54c3d5ab8
jellest <jellejstuurman@gmail.com>
2019-01-15 18:38:15 +0100

    import ahn improvements

--- R/multiple_ahn.R
+++ R/multiple_ahn.R
@@ -1,6 +1,9 @@
 aws_list <- dplyr::filter(AWS.df, DS_DESC == "AWS" & Aparatuur == "site")[,1]
 
-multiple_ahn <- function(list){
+multiple_ahn <- function(list, delete_sheets){
+  if(missing(delete_sheets)){
+    delete_sheets = FALSE
+  }
   start_time <- Sys.time()
   for (a in 1:length(list)){
     spatialpoint.sp <- select_single_aws(aws.df = AWS.df, aws_name = list[a], sensor_name = "temp_150cm")[["aws_rd.sp"]]
@@ -10,11 +13,11 @@ multiple_ahn <- function(list){
                resolution = 0.5, radius = 500,
                raw_ahn = TRUE, terrain_ahn = TRUE,
                AHN3 = FALSE,
-               delete_sheets = TRUE)
+               delete_sheets = delete_sheets)
   }
   end_time <- Sys.time()
   elapsed_time <- ceiling(end_time - start_time)
   message(paste("Finished importing multiple AHN data sets. Elapsed Time:", elapsed_time, "seconds."))
 }
 
-multiple_ahn(c("De Bilt"))
+multiple_ahn(c("De Bilt"), delete_sheets = TRUE)
